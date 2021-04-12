cb06dd22d71349d146b56a5e2f7f57c514c57cee
nuengla <81779600+nuengla@users.noreply.github.com>
2021-04-13 00:01:37 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      
+      <h1>Hello World</h1>
+      <p>สวัสดี ชาวโลก</p>
+        
+      </div>
+  );
+}
+
+export default App;
